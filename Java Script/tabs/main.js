f82faee8345cd4aa20a8cc3d04d6a2bb1268b8ab
zeyad belal <zeyadbelal00@gmail.com>
2023-08-10 15:41:58 +0300

    fix

--- Java Script/tabs/main.js
+++ Java Script/tabs/main.js
@@ -1,17 +1,33 @@
-let tabs = document.querySelectorAll(".tabs li");
+// let tabs = document.querySelectorAll(".tabs li");
 
-let content = document.querySelectorAll(".content > div");
+// let content = document.querySelectorAll(".content > div");
 
-tabs.forEach((ele) => {
-  ele.addEventListener("click", (e) => {
-    tabs.forEach((ele) => {
-      ele.classList.remove("active");
-    });
-    e.currentTarget.classList.add("active");
+// tabs.forEach((ele) => {
+//   ele.addEventListener("click", (e) => {
+//     tabs.forEach((ele) => {
+//       ele.classList.remove("active");
+//     });
+//     e.currentTarget.classList.add("active");
 
-    content.forEach((ele) => {
-      ele.style.display = "none";
-    });
-    document.querySelector(e.currentTarget.dataset.num).style.display = "block";
-  });
-});
+//     content.forEach((ele) => {
+//       ele.style.display = "none";
+//     });
+//     document.querySelector(e.currentTarget.dataset.num).style.display = "block";
+//   });
+// });
+
+const arr1 = [6,4,3,2,1,7] 
+
+function findPairSum(arr,sum){
+  let mySet= new Set()
+
+  for(let i = 0; i< arr.length; i++){
+    if(mySet.has(sum - arr[i])){
+      return true
+    }
+    mySet.add(arr[i])
+  }
+  return false
+}
+
+console.log(findPairSum(arr1,9))
